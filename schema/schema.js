ebbadf703af43f80990d46f74024efeba4fa11ec
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2023-10-15 17:49:37 +0300

    resolve func. and first query

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,32 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+//dummy data
+const movies = [
+  {
+    id: '1',
+    title: 'The Godfather',
+    description:
+      'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    year: 1972,
+  },
+  {
+    id: '2',
+    title: 'Scarface',
+    description:
+      'In Miami in 1980, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.',
+    year: 1980,
+  },
+  {
+    id: '3',
+    title: 'Pulp Fiction',
+    description:
+      "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    year: 1994,
+  },
+];
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -20,6 +46,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db / other source
+        return _.find(movies, { id: args.id });
       },
     },
   },
